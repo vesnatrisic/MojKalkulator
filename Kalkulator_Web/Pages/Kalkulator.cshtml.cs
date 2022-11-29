b3cade070ac773e8d25472057c08429eb1a52b2f
vesnatrisic <trisic.vesna1@gmail.com>
2022-11-29 13:26:40 +0100

    Promjene na Webu

---
+++ Kalkulator_Web/Pages/Kalkulator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kalkulator_Web.Pages
+{
+    public class KalkulatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
